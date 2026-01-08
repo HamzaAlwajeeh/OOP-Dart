9fdbd00fcd1a14c3ffe9f67f19f2d59969a40c82
HamzaAlwajeeh <ha20mza60@gmail.com>
2026-01-08 17:31:11 +0300

    Classes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,14 @@
 void main() {
   print('Learning OOP');
 }
+
+// [1] Create a class => Human
+// [2] Create an attributes => skinColor , hight , wight , blood type
+
+class Human {
+  String? skinColor;
+  int? hight;
+  int? wight;
+  String? bloodType;
+  int numberOfarms = 2;
+}
