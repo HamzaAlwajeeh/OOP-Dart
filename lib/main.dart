090c7e935d4bfc9bd4be1c78de149e38a3d4a9ca
HamzaAlwajeeh <ha20mza60@gmail.com>
2026-01-08 17:33:36 +0300

    Objects

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,23 @@
 void main() {
-  print('Learning OOP');
+  Human hamza = Human();
+  hamza.skinColor = 'White';
+  hamza.hight = 163;
+  hamza.wight = 48;
+  hamza.bloodType = 'A+';
+  print("Skin Color: ${hamza.skinColor}");
+  print("Hight: ${hamza.hight}");
+  print("Wight: ${hamza.wight}");
+  print("Blood Type: ${hamza.bloodType}");
+
+  Human ali = Human();
+  ali.skinColor = 'Black';
+  ali.hight = 180;
+  ali.wight = 80;
+  ali.bloodType = 'O';
+  print("Skin Color: ${ali.skinColor}");
+  print("Hight: ${ali.hight}");
+  print("Wight: ${ali.wight}");
+  print("Blood Type: ${ali.bloodType}");
 }
 
 // [1] Create a class => Human
